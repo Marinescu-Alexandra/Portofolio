26ddc281a4d0cf638bb9f0cdc1aaf674813e01bb
Ale <mihaela.marinescu@s.unibuc.ro>
2024-01-15 13:47:51 +0200

    small visual change on project card

--- src/components/projects/projectCard.tsx
+++ src/components/projects/projectCard.tsx
@@ -15,7 +15,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ title, link, info, icons, image }) 
         <>
             <div className="w-full flex flex-row justify-center items-center sm-desktop:flex-col">
                 <motion.div className="w-[50%] bg-dark h-[500px] rounded-l-2xl flex items-center justify-center dark:bg-lightDark
-                sm-desktop:w-[90%] sm-desktop:rounded-t-2xl sm-desktop:rounded-b-none sm-desktop:min-h-auto shadow-glow"
+                sm-desktop:w-[90%] sm-desktop:h-[300px] sm-desktop:rounded-t-2xl sm-desktop:rounded-b-none sm-desktop:min-h-auto shadow-glow"
                     initial={{ boxShadow: '0 0 5px #65ddf4' }}
                     animate={{ boxShadow: '0 0 30px #65ddf4' }}
                     transition={{
@@ -36,7 +36,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ title, link, info, icons, image }) 
                     />
                 </motion.div>
                 <motion.div className="w-[36%] h-[500px] rounded-r-2xl flex flex-col justify-around items-left sm-desktop:w-[90%]
-                sm-desktop:rounded-t-none sm-desktop:rounded-b-2xl sm-desktop:h-auto shadow-glow bg-white dark:bg-dark"
+                sm-desktop:rounded-t-none sm-desktop:rounded-b-2xl sm-desktop:h-auto shadow-glow bg-white dark:bg-dark gap-2" 
                     initial={{ boxShadow: '0 0 5px #65ddf4' }}
                     animate={{ boxShadow: '0 0 22px #65ddf4' }}
                     transition={{
@@ -67,7 +67,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ title, link, info, icons, image }) 
                             />
                         )}
                     </div>
-                    <div className="flex text-medium font-medium text-light mx-6 mb-6 dark:text-dark">
+                    <div className="flex text-normal font-medium text-light mx-6 mb-6 dark:text-dark">
                         <a className="rounded-lg bg-dark px-2 py-1 left-0 dark:bg-black dark:text-light dark:border-2 dark:border-light" href={link}>
                             See full details on GitHub
                         </a>
